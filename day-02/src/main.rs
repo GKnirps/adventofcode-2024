87a766b2e612b7f01226ef41dc9fc41dc5b50119
Guido Knips <guido.knips@posteo.de>
2024-12-02 07:23:03 +0100

    Add solution for day 2, part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -13,6 +13,9 @@ fn main() -> Result<(), String> {
     let safe_reports = count_safe(&reports);
     println!("{safe_reports} are safe");
 
+    let dampened_safe_reports = count_dampened_safe(&reports);
+    println!("With the problem dampener {dampened_safe_reports} reports are safe.");
+
     Ok(())
 }
 
@@ -21,11 +24,49 @@ fn count_safe(reports: &[Box<[u32]>]) -> usize {
 }
 
 fn is_safe(report: &[u32]) -> bool {
-    (report.windows(2).all(|pair| pair[0] < pair[1])
-        || report.windows(2).all(|pair| pair[1] < pair[0]))
-        && report.windows(2).all(|pair| {
-            let diff = pair[0].max(pair[1]) - pair[0].min(pair[1]);
-            diff > 0 && diff < 4
+    let (c1, c2) = dampener_candidates(report);
+    c1.is_none() || c2.is_none()
+}
+
+fn dampener_candidates(report: &[u32]) -> (Option<usize>, Option<usize>) {
+    let candidate_1 = report
+        .windows(2)
+        .enumerate()
+        .filter(|(_, pair)| pair[0] >= pair[1] || pair[1] - pair[0] > 3)
+        .map(|(i, _)| i)
+        .next();
+    let candidate_2 = report
+        .windows(2)
+        .enumerate()
+        .filter(|(_, pair)| pair[1] >= pair[0] || pair[0] - pair[1] > 3)
+        .map(|(i, _)| i)
+        .next();
+    (candidate_1, candidate_2)
+}
+
+fn count_dampened_safe(reports: &[Box<[u32]>]) -> usize {
+    reports
+        .iter()
+        .filter(|report| is_dampened_safe(report))
+        .count()
+}
+
+fn is_dampened_safe(report: &[u32]) -> bool {
+    let (c1, c2) = dampener_candidates(report);
+    if c1.is_none() || c2.is_none() {
+        return true;
+    }
+    c1.iter()
+        .flat_map(|i| [*i, i + 1])
+        .chain(c2.iter().flat_map(|i| [*i, i + 1]))
+        .any(|unsafe_i| {
+            let dampened_report: Box<[u32]> = report
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| *i != unsafe_i)
+                .map(|(_, level)| *level)
+                .collect();
+            is_safe(&dampened_report)
         })
 }
 
@@ -46,7 +87,7 @@ fn parse_report(line: &str) -> Result<Box<[u32]>, String> {
 mod test {
     use super::*;
 
-    static input: &str = r#"7 6 4 2 1
+    static INPUT: &str = r#"7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
@@ -57,12 +98,84 @@ mod test {
     #[test]
     fn count_safe_counts_correctly() {
         // given
-        let reports = parse(input).expect("expected successful parsing");
+        let reports = parse(INPUT).expect("expected successful parsing");
 
         // when
         let count = count_safe(&reports);
 
         // then
         assert_eq!(count, 2);
+    }
+
+    #[test]
+    fn count_dampened_safe_counts_correctly() {
+        // given
+        let reports = parse(INPUT).expect("expected successful parsing");
+
+        // when
+        let count = count_dampened_safe(&reports);
+
+        // then
+        assert_eq!(count, 4);
+    }
+
+    #[test]
+    fn is_dampened_safe_works_if_first_index_needs_to_be_removed() {
+        // given
+        let report = &[1, 2, 4, 3, 4];
+
+        // when
+        let safe = is_dampened_safe(report);
+
+        // then
+        assert!(safe);
+    }
+
+    #[test]
+    fn is_dampened_safe_works_if_first_report_is_in_wrong_order() {
+        // given
+        let report = &[2, 1, 2, 3, 4];
+
+        // when
+        let safe = is_dampened_safe(report);
+
+        // then
+        assert!(safe);
+    }
+
+    #[test]
+    fn is_dampened_safe_works_if_first_report_is_too_far_away() {
+        // given
+        let report = &[1, 8, 9, 10];
+
+        // when
+        let safe = is_dampened_safe(report);
+
+        // then
+        assert!(safe);
+    }
+
+    #[test]
+    fn is_dampened_safe_works_if_last_report_is_in_wrong_order() {
+        // given
+        let report = &[1, 2, 3, 4, 3];
+
+        // when
+        let safe = is_dampened_safe(report);
+
+        // then
+        assert!(safe);
+    }
+
+    #[test]
+    fn is_dampened_safe_works_if_last_report_is_too_far_away() {
+        // given
+        let report = &[1, 2, 3, 4, 8];
+
+        // when
+        let safe = is_dampened_safe(report);
+
+        // then
+        assert!(safe);
     }
 }
