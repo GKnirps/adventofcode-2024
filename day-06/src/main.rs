4326f5fe58ff951b4970f6e4a620cb9ac62405c5
Guido Knips <guido.knips@posteo.de>
2024-12-06 07:48:24 +0100

    Add solution for day 6, part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -10,13 +10,52 @@ fn main() -> Result<(), String> {
     let content = read_to_string(Path::new(&filename)).map_err(|e| e.to_string())?;
     let map = parse(&content)?;
 
-    let distinct_guard_positions = track_guard(&map);
+    let distinct_guard_positions = track_guard(&map).len();
     println!("The guard visits {distinct_guard_positions} tiles before leaving the map");
+
+    let obstructions = obstruction_candidates(&map);
+    println!("{obstructions} different positions can be chosen for an obstruction");
 
     Ok(())
 }
 
-fn track_guard(map: &Map) -> usize {
+fn obstruction_candidates(map: &Map) -> usize {
+    // Hello brute force my old friend
+    track_guard(map)
+        .iter()
+        .filter(|obstruction_pos| {
+            let mut rocks = map.rocks.clone();
+            rocks.insert(**obstruction_pos);
+            let updated_map = Map { rocks, ..*map };
+            has_loop(&updated_map)
+        })
+        .count()
+}
+
+fn has_loop(map: &Map) -> bool {
+    let mut pos = map.guard;
+    let mut dir: (i32, i32) = (0, -1);
+    let mut visited: HashSet<(i32, i32, i32, i32)> =
+        HashSet::with_capacity((map.width * map.height) as usize);
+
+    while pos.0 >= 0 && pos.1 >= 0 && pos.0 < map.width && pos.1 < map.height {
+        if visited.contains(&(pos.0, pos.1, dir.0, dir.1)) {
+            return true;
+        }
+        visited.insert((pos.0, pos.1, dir.0, dir.1));
+        if map.rocks.contains(&(pos.0 + dir.0, pos.1 + dir.1)) {
+            let odir = dir;
+            dir.0 = -odir.1;
+            dir.1 = odir.0;
+        } else {
+            pos.0 += dir.0;
+            pos.1 += dir.1;
+        }
+    }
+    false
+}
+
+fn track_guard(map: &Map) -> HashSet<(i32, i32)> {
     let mut pos = map.guard;
     let mut dir: (i32, i32) = (0, -1);
     let mut visited: HashSet<(i32, i32)> =
@@ -34,7 +73,7 @@ fn track_guard(map: &Map) -> usize {
         }
     }
 
-    visited.len()
+    visited
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -113,6 +152,18 @@ mod test {
         let positions = track_guard(&map);
 
         // then
-        assert_eq!(positions, 41);
+        assert_eq!(positions.len(), 41);
+    }
+
+    #[test]
+    fn obstruction_candidates_works_for_example() {
+        // given
+        let map = parse(EXAMPLE).expect("expected exampe input to parse");
+
+        // when
+        let obstructions = obstruction_candidates(&map);
+
+        // then
+        assert_eq!(obstructions, 6);
     }
 }
