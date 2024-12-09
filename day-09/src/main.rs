5ca81e78d05c98d13cb233e4b7cf2b6c7985a83b
Guido Knips <guido.knips@posteo.de>
2024-12-09 07:16:20 +0100

    Add solution for day 9, part 2
    Pretty ugly solution, but I'm not feeling well today.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -10,14 +10,75 @@ fn main() -> Result<(), String> {
     let original_disk = parse(&content)?;
 
     let fragmented_checksum = fragment_disk(&original_disk);
-    println!("The compacted hard drive's checksum os {fragmented_checksum}");
+    println!("The compacted hard drive's checksum is {fragmented_checksum}");
+
+    let defragmented_checksum = defragment_disk(&original_disk);
+    println!("The defragmented hard drive's checksum is {defragmented_checksum}");
 
     Ok(())
 }
 
+fn defragment_disk(original_disk: &[u8]) -> u64 {
+    /*let mut compressed_disk: Box<[(u8, Option<u16>)]> = original_disk
+    .iter()
+    .enumerate()
+    .map(|(i, size)| {
+        (
+            *size,
+            if i % 2 == 0 {
+                Some((i / 2) as u16)
+            } else {
+                None
+            },
+        )
+    })
+    .collect();*/
+    let mut disk = decompress_disk(original_disk);
+    let mut u: usize = disk.len() - 1;
+    let mut max_id: u16 = (original_disk.len() / 2) as u16;
+    while u > 0 {
+        while disk[u].map(|id| id > max_id).unwrap_or(true) {
+            u -= 1;
+        }
+        let mut ul = u;
+        while ul > 0 && disk[ul] == disk[u] {
+            ul -= 1;
+        }
+        let size = u - ul;
+        if let Some(target_offset) = find_space(&disk[0..=ul], size) {
+            for i in 0..size {
+                disk[target_offset + i] = disk[ul + 1 + i];
+                disk[ul + 1 + i] = None;
+            }
+        }
+        max_id = max_id.max(1) - 1;
+        u = ul;
+    }
+
+    disk_checksum(&disk)
+}
+
+// return offset of first space available that is large enough to fit len, or None if there is no
+// such space
+fn find_space(disk: &[Option<u16>], len: usize) -> Option<usize> {
+    if len == 0 {
+        return None;
+    }
+    let mut offset = 0;
+    while offset + len <= disk.len() {
+        if disk[offset].is_some() {
+            offset += 1;
+        } else {
+            if (offset..(offset + len)).all(|i| disk[i].is_none()) {
+                return Some(offset);
+            }
+            offset += 1;
+        }
+    }
+    None
+}
+
 fn fragment_disk(original_disk: &[u8]) -> u64 {
-    // let's do the brute force again. This will probably byte me in the ass in part 2, but
-    // whatever
     let mut disk = decompress_disk(original_disk);
     let mut l: usize = 0;
     let mut u: usize = disk.len() - 1;
@@ -101,5 +162,17 @@ mod test {
 
         // then
         assert_eq!(checksum, 1928);
+    }
+
+    #[test]
+    fn defragment_disk_works_for_example() {
+        // given
+        let original_disk = parse(EXAMPLE).expect("expected example input to parse");
+
+        // when
+        let checksum = defragment_disk(&original_disk);
+
+        // then
+        assert_eq!(checksum, 2858);
     }
 }
