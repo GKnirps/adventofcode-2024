3d8be094c1e28d6a660ecd8d7a3d8592b6420892
Guido Knips <guido.knips@posteo.de>
2024-12-20 09:10:31 +0100

    Add solution for day 20, part 2

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -12,54 +12,120 @@ fn main() -> Result<(), String> {
     let content = read_to_string(Path::new(&filename)).map_err(|e| e.to_string())?;
     let (track, start, goal) = parse(&content)?;
 
-    let shortcuts = find_path_with_cheat(&track, start, goal);
+    let shortcuts = find_path_with_cheat(&track, start, goal, 2);
     let n: usize = shortcuts
         .iter()
         .filter(|(dist, _)| **dist >= 100)
         .map(|(_, n)| *n)
         .sum();
-    println!("There are {n} cheats that save at least 100 picosedonds");
+    println!(
+        "There are {n} cheats of up to 2 picosecond length that save at least 100 picoseconds"
+    );
+
+    let shortcuts = find_path_with_cheat(&track, start, goal, 20);
+    let n: usize = shortcuts
+        .iter()
+        .filter(|(dist, _)| **dist >= 100)
+        .map(|(_, n)| *n)
+        .sum();
+    println!(
+        "There are {n} cheats of up to 20 picosecond length that save at least 100 picoseconds"
+    );
 
     Ok(())
 }
 
-fn find_path_with_cheat(track: &Track, start: V2, goal: V2) -> HashMap<u64, usize> {
-    let uncheated_dist = match find_path(track, start, goal) {
-        Some(dist) => dist,
-        None => {
-            return HashMap::new();
-        }
+fn find_path_with_cheat(
+    track: &Track,
+    start: V2,
+    goal: V2,
+    cheat_time: u64,
+) -> HashMap<u64, usize> {
+    let uncheated_dist = find_path(track, start);
+    let uncheated_shortest_path = match uncheated_dist.get(&goal) {
+        Some(d) => d,
+        None => return HashMap::new(),
     };
-    let mut shortcuts: HashMap<u64, usize> = HashMap::with_capacity(track.tiles.len());
-    // brute force, but still faster than the fancy algorithm I tried first
-    for dist in (1..track.height - 1)
-        .flat_map(|y| (1..track.width - 1).map(move |x| (x, y)))
-        .filter_map(|(x, y)| {
-            if track.get((x, y)) == Tile::Wall {
-                let mut cheat_track = track.clone();
-                cheat_track.tiles[x + y * track.width] = Tile::Floor;
-                Some(cheat_track)
-            } else {
-                None
+    let uncheated_dist_inverse = find_path(track, goal);
+    let mut cheated_dists: HashMap<u64, usize> = HashMap::with_capacity(1024);
+
+    for (i, tile) in track.tiles.iter().enumerate() {
+        if *tile == Tile::Floor {
+            let x = i % track.width;
+            let y = i / track.width;
+            let uncheated_from = match uncheated_dist.get(&(x, y)) {
+                Some(d) => d,
+                None => {
+                    continue;
+                }
+            };
+            for cheated_distance in 2..=cheat_time {
+                for dy in 0..=cheated_distance {
+                    let dx = cheated_distance - dy;
+                    for (cheat_x, cheat_y) in cheatable_tiles(track, x, y, dx as usize, dy as usize)
+                        .iter()
+                        .filter_map(|t| *t)
+                    {
+                        if let Some(from_cheated) = uncheated_dist_inverse.get(&(cheat_x, cheat_y))
+                        {
+                            let total_cheated_distance =
+                                uncheated_from + cheated_distance + from_cheated;
+                            if total_cheated_distance < *uncheated_shortest_path {
+                                *cheated_dists
+                                    .entry(uncheated_shortest_path - total_cheated_distance)
+                                    .or_insert(0) += 1;
+                            }
+                        }
+                    }
+                }
             }
-        })
-        .filter_map(|track| find_path(&track, start, goal))
-    {
-        if uncheated_dist > dist {
-            *shortcuts.entry(uncheated_dist - dist).or_insert(0) += 1;
         }
     }
-    shortcuts
+    cheated_dists
 }
 
-fn find_path(track: &Track, start: V2, goal: V2) -> Option<u64> {
+fn cheatable_tiles(track: &Track, x: usize, y: usize, dx: usize, dy: usize) -> [Option<V2>; 4] {
+    let mut result: [Option<V2>; 4] = [None; 4];
+    if dx < x {
+        if dy < y {
+            let cheat_x = x - dx;
+            let cheat_y = y - dy;
+            if track.get((cheat_x, cheat_y)) == Tile::Floor {
+                result[0] = Some((cheat_x, cheat_y));
+            }
+        }
+        if y + dy < track.height - 1 && dy != 0 {
+            let cheat_x = x - dx;
+            let cheat_y = y + dy;
+            if track.get((cheat_x, cheat_y)) == Tile::Floor {
+                result[1] = Some((cheat_x, cheat_y));
+            }
+        }
+    }
+    if dx + x < track.width - 1 && dx != 0 {
+        if dy < y {
+            let cheat_x = x + dx;
+            let cheat_y = y - dy;
+            if track.get((cheat_x, cheat_y)) == Tile::Floor {
+                result[2] = Some((cheat_x, cheat_y));
+            }
+        }
+        if y + dy < track.height - 1 && dy != 0 {
+            let cheat_x = x + dx;
+            let cheat_y = y + dy;
+            if track.get((cheat_x, cheat_y)) == Tile::Floor {
+                result[3] = Some((cheat_x, cheat_y));
+            }
+        }
+    }
+    result
+}
+
+fn find_path(track: &Track, start: V2) -> HashMap<V2, u64> {
     let mut seen: HashMap<V2, u64> = HashMap::with_capacity(track.tiles.len());
     let mut queue: VecDeque<(usize, usize, u64)> = VecDeque::with_capacity(track.tiles.len());
     queue.push_back((start.0, start.1, 0));
     while let Some((x, y, dist)) = queue.pop_front() {
-        if x == goal.0 && y == goal.1 {
-            return Some(dist);
-        }
         if seen.contains_key(&(x, y)) {
             continue;
         }
@@ -77,7 +143,7 @@ fn find_path(track: &Track, start: V2, goal: V2) -> Option<u64> {
             queue.push_back((x, y + 1, dist + 1));
         }
     }
-    None
+    seen
 }
 
 type V2 = (usize, usize);
@@ -187,10 +253,9 @@ mod test {
         let (track, start, goal) = parse(EXAMPLE).expect("expected example input to parse");
 
         // when
-        let shortcuts = find_path_with_cheat(&track, start, goal);
+        let shortcuts = find_path_with_cheat(&track, start, goal, 2);
 
         // then
-        println!("{shortcuts:?}");
         assert_eq!(shortcuts.len(), 11);
         assert_eq!(shortcuts.get(&2), Some(&14));
         assert_eq!(shortcuts.get(&4), Some(&14));
@@ -203,5 +268,37 @@ mod test {
         assert_eq!(shortcuts.get(&38), Some(&1));
         assert_eq!(shortcuts.get(&40), Some(&1));
         assert_eq!(shortcuts.get(&64), Some(&1));
+    }
+
+    #[test]
+    fn find_path_with_cheat_works_with_20_ps_for_example() {
+        // given
+        let (track, start, goal) = parse(EXAMPLE).expect("expected example input to parse");
+
+        // when
+        let shortcuts = find_path_with_cheat(&track, start, goal, 20);
+
+        // then
+        let shortcuts_50: HashMap<u64, usize> = shortcuts
+            .iter()
+            .filter(|(dist, _)| **dist >= 50)
+            .map(|(d, n)| (*d, *n))
+            .collect();
+        println!("{shortcuts_50:?}");
+        assert_eq!(shortcuts_50.len(), 14);
+        assert_eq!(shortcuts_50.get(&50), Some(&32));
+        assert_eq!(shortcuts_50.get(&52), Some(&31));
+        assert_eq!(shortcuts_50.get(&54), Some(&29));
+        assert_eq!(shortcuts_50.get(&56), Some(&39));
+        assert_eq!(shortcuts_50.get(&58), Some(&25));
+        assert_eq!(shortcuts_50.get(&60), Some(&23));
+        assert_eq!(shortcuts_50.get(&62), Some(&20));
+        assert_eq!(shortcuts_50.get(&64), Some(&19));
+        assert_eq!(shortcuts_50.get(&66), Some(&12));
+        assert_eq!(shortcuts_50.get(&68), Some(&14));
+        assert_eq!(shortcuts_50.get(&70), Some(&12));
+        assert_eq!(shortcuts_50.get(&72), Some(&22));
+        assert_eq!(shortcuts_50.get(&74), Some(&4));
+        assert_eq!(shortcuts_50.get(&76), Some(&3));
     }
 }
